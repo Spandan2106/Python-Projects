688ae4f98f55db13402d318549e3fb2c1b5ff8d8
Spandan2106 <sd672483@gmail.com>
2025-07-30 21:58:58 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,11 @@
+#  python code to swap two numbers
+print("Welcome to the Number Swapper!"
+      "\nThis program will swap two numbers for you."
+      "\nPlease enter two numbers to swap:")
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+a,b = b,a
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
+print("Thank you for using the Number Swapper!")
